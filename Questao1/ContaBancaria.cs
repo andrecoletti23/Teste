0e81e2d99dd64a0e2983a073d19cc1de22d3e778
andrecoletti23 <andre.bongo23@gmail.com>
2023-02-10 10:53:14 -0300

    Teste

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria {
+      public class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+        private const double TaxaSaque = 3.50;
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public Conta(int numero, string titular) : this(numero, titular, 0) { }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+ 
+            Saldo -= valor + TaxaSaque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular + ", Saldo: $ " + Saldo.ToString("F2");
+        }
+       
+    }
+}
---
+++ Questao2/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+
+public class Program
+{
+    static void Main(string[] args)
+        {
+            Console.WriteLine(GetGoalsByTeamAndYear("Paris Saint-Germain", 2013).Result);
+            Console.WriteLine(GetGoalsByTeamAndYear("Chelsea", 2014).Result);
+            Console.ReadLine();
+        }
+
+        static async Task<string> GetGoalsByTeamAndYear(string teamName, int year)
+        {
+            int goalsScored = 0;
+            int page = 1;
+            bool moreData = true;
+
+            while (moreData)
+            {
+                string url = $"https://jsonmock.hackerrank.com/api/football_matches?team1={teamName}&year={year}&page={page}";
+
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        dynamic data = JsonConvert.DeserializeObject(result);
+
+                        if (data.data.Count == 0)
+                        {
+                            moreData = false;
+                        }
+                        else
+                        {
+                            foreach (var match in data.data)
+                            {
+                                if (match.team1 == teamName)
+                                {
+                                    goalsScored += match.goals1;
+                                }
+                                else if (match.team2 == teamName)
+                                {
+                                    goalsScored += match.goals2;
+                                }
+                            }
+
+                            page++;
+                        }
+                    }
+                    else
+                    {
+                        return "Request failed";
+                    }
+                }
+            }
+
+            return $"Team {teamName} scored {goalsScored} goals in {year}";
+        }
+}
